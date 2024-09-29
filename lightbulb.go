8375516e5b98b071b4c5ff7018d00a7f52636880
Melody Asper <melodyaheath@gmail.com>
2024-09-29 09:31:58 +0200

    Refactor states to only hold neccessary data.

--- lightbulb.go
+++ lightbulb.go
@@ -2,85 +2,13 @@ package main
 
 import "fmt"
 
-/* State */
-type StateData struct {
-	Fitted        bool
-	IsOn          bool
-	RemainingUses uint
-	IsBlown       bool
-}
-
-type State interface {
-	GetData() *StateData
-	IsValid() bool
-}
-
-type StateNotFitted struct {
-	data StateData
-}
-
-type StateWorking struct {
-	data StateData
-}
-
-type StateBlown struct {
-	data StateData
-}
-
-type Aggregate interface {
-	decide(Command) []Event
-	evolve(Event)
-}
-type Bulb struct {
-	state State
-}
-
-
-
-func (s StateNotFitted) GetData() *StateData {
-	return &s.data
-}
-func (s StateWorking) GetData() *StateData {
-	return &s.data
-}
-func (s StateBlown) GetData() *StateData {
-	return &s.data
-}
-
-func (s StateNotFitted) IsValid() bool {
-	data := s.GetData()
-	return !data.Fitted && !data.IsBlown && data.RemainingUses == 0
-}
-
-func (s StateWorking) IsValid() bool {
-	data := s.GetData()
-	return data.Fitted && !data.IsBlown
-}
-
-func (s StateBlown) IsValid() bool {
-	data := s.GetData()
-	return data.Fitted && data.IsBlown  && !data.IsOn && data.RemainingUses == 0
-}
-
-
-// Event with isEvent marker method.
+/************************ EVENTS  ****************/
+// Event interface with isEvent marker method.
 type Event interface {
 	isEvent()
 }
 
-// Command with isCommand marker method.
-type Command interface {
-	isCommand() 
-}
-
-/* Commands */
-type CommandFit struct {
-	MaxUses uint
-}
-type CommandSwitchOn struct{}
-type CommandSwitchOff struct{}
-
-/* Events */
+// Event defintions
 type EventFitted struct {
 	MaxUses uint
 }
@@ -88,20 +16,94 @@ type EventSwitchedOn struct{}
 type EventSwitchedOff struct{}
 type EventBlew struct{}
 
-/* Event markers */
+// Event markers
 func (e EventFitted) isEvent() {}
 func (e EventSwitchedOn) isEvent() {}
 func (e EventSwitchedOff) isEvent() {}
 func (e EventBlew) isEvent() {}
 
-/* Command marker method implementations */
+// Implement the Stringer (%s formatter) interfaces for Events
+func (e EventFitted) String() string {
+	return fmt.Sprintf("EventFitted {MaxUses: %d}", e.MaxUses)
+}
+func (e EventSwitchedOn) String() string {
+	return "EventSwitchedOn {}"
+}
+func (e EventSwitchedOff) String() string {
+	return "EventSwitchedOff {}"
+}
+func (e EventBlew) String() string {
+	return "EventSwitchedOff {}"
+}
+
+
+/************************ STATES  ****************/
+// State interface with marker function
+type State interface {
+	isState()
+}
+
+// State definitions
+type StateNotFitted struct {}
+type StateWorking struct {
+	IsOn          bool
+	RemainingUses uint
+}
+
+type StateBlown struct {}
+
+// State marker implementation
+func (s StateNotFitted) isState() {}
+func (s StateWorking) isState() {}
+func (s StateBlown) isState() {}
+
+// Implement the Stringer (%s formatter) interfaces for States
+func (s StateNotFitted) String() string {
+	return "StateNotFitted {}"
+}
+func (s StateWorking) String() string {
+	return fmt.Sprintf("StateWorking {IsOn: %t, RemainingUses: %d}", s.IsOn, s.RemainingUses)
+}
+func (s StateBlown) String() string {
+	return "StateBlown {}"
+}
+
+/************************ COMMANDS ****************/
+// Command interface isCommand marker method.
+type Command interface {
+	isCommand() 
+}
+// Command Defintions
+type CommandFit struct {
+	MaxUses uint
+}
+type CommandSwitchOn struct{}
+type CommandSwitchOff struct{}
+
+// Command marker method implementations
 func (c CommandFit) isCommand() {}
 func (c CommandSwitchOn) isCommand() {}
 func (c CommandSwitchOff) isCommand() {}
 
-func initial_state() State {
-	state_data := StateData{Fitted: false, IsOn: false, RemainingUses: 0}
-	return StateNotFitted{state_data}
+// Implement the Stringer (%s formatter) interfaces for Commands
+func (c CommandFit) String() string {
+	return fmt.Sprintf("CommandFit {MaxUses: %d}", c.MaxUses)
+}
+func (s CommandSwitchOn) String() string {
+	return "CommandSwitchOn {}"
+}
+func (s CommandSwitchOff) String() string {
+	return "CommandSwitchOff {}"
+}
+
+
+/************************ AGGREGATE AND BULB **************/
+type Aggregate interface {
+	decide(Command) []Event
+	evolve(Event)
+}
+type Bulb struct {
+	state State
 }
 
 func (b Bulb) decide(c Command) []Event {
@@ -112,9 +114,8 @@ func (b Bulb) decide(c Command) []Event {
 		}
 	case CommandSwitchOn:
 		if sv, sok := b.state.(StateWorking); sok {
-			data := sv.GetData()
-			if !data.IsOn {
-				if data.RemainingUses > 0 {
+			if !sv.IsOn {
+				if sv.RemainingUses > 0 {
 					return []Event{EventSwitchedOn{}}
 				}
 				return []Event{EventBlew{}}
@@ -122,7 +123,7 @@ func (b Bulb) decide(c Command) []Event {
 		}
 	case CommandSwitchOff:
 		if sv, sok := b.state.(StateWorking); sok {
-			if sv.GetData().IsOn {
+			if sv.IsOn {
 				return []Event{EventSwitchedOff{}}
 			}
 		}
@@ -131,33 +132,41 @@ func (b Bulb) decide(c Command) []Event {
 }
 func (b *Bulb) evolve(e Event) {
 	/* Clone data from pointer */
-	new_data := *(b.state.GetData())
-
 	switch ev := e.(type) {
 		case EventFitted:
-			new_data.Fitted = true
-			new_data.RemainingUses = ev.MaxUses
-			b.state = StateWorking{new_data}
+			new_state := StateWorking { RemainingUses: ev.MaxUses, IsOn: false}
+			b.state = new_state
 			return
 
 		case EventSwitchedOn:
-			new_data.RemainingUses -= 1
-			new_data.IsOn = true
-			b.state = StateWorking{new_data}
+			if sv, sok := b.state.(StateWorking); sok {
+				new_state := sv
+				new_state.IsOn = true
+				new_state.RemainingUses -= 1
+				b.state = new_state
+			}
 			return
 
 		case EventSwitchedOff:
-			new_data.IsOn = false
-			b.state = StateWorking{new_data}
+			if sv, sok := b.state.(StateWorking); sok {
+				new_state := sv
+				new_state.IsOn = false
+				b.state = new_state
+			}
 			return
 
 		case EventBlew:
-			new_data.IsBlown = true
-			new_data.IsOn = false
-			b.state = StateBlown{new_data}
+			b.state = StateBlown{}
 			return
 	}
 }
+
+
+// Helper function
+func initial_state() State {
+	return StateNotFitted{}
+}
+
 
 func main() {
 	// Setup commands
@@ -167,18 +176,15 @@ func main() {
 	commands := []Command{command_fit, command_switch_on, command_switch_off, command_switch_on}
 	bulb := Bulb{state: initial_state()}
 	for idx, command := range commands {
-		fmt.Println("---------------")
-		fmt.Println("Evaluating command ", idx)
+		fmt.Printf("Evaluating command: %s\n", command)
 		events := bulb.decide(command)
 		if len(events) == 0 && idx < (len(commands) - 1) {
 			fmt.Println("Unexpected end state")
 		}
 		for _, event := range events {
+			fmt.Printf("Event             : %s\n", event)
 			bulb.evolve(event)
-			fmt.Println("Is bulb state valid:", bulb.state.IsValid())
-			fmt.Println("Is on              :", bulb.state.GetData().IsOn)
-			fmt.Println("Uses remaining     :", bulb.state.GetData().RemainingUses)
-			fmt.Println("Is bulb blown      :", bulb.state.GetData().IsBlown)
+			fmt.Printf("Results in state  : %s\n", bulb.state)
 		}
 	}
 	
